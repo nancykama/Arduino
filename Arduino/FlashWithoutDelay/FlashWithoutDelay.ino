61812010e4f13331c6b0e701febc9b98edcb7a26
Nancy Kama <70664950+nancykama@users.noreply.github.com>
2022-06-15 21:01:27 -0700

    Update FlashWithoutDelay.ino

--- Arduino/FlashWithoutDelay/FlashWithoutDelay.ino
+++ Arduino/FlashWithoutDelay/FlashWithoutDelay.ino
@@ -4,7 +4,7 @@ int ledPin = 13; // the number of the LED pin
 int ledState = LOW; // ledState used to set the LED
 unsigned long previousMillis = 0; // will store last time LED was updated
 long OnTime = 250; // milliseconds of on-time
-long OffTime = 750; // milliseconds of off-time
+long OffTime = 650; // milliseconds of off-time
 
 void setup() {
 // set the digital pin as output:
