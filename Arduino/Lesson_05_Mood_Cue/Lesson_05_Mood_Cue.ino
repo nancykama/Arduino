c7f21cb974de4b3d2963a4c1021b6a01fdc5d07f
Nancy Kama <70664950+nancykama@users.noreply.github.com>
2022-06-15 21:03:53 -0700

    Update Lesson_05_Mood_Cue.ino

--- Arduino/Lesson_05_Mood_Cue/Lesson_05_Mood_Cue.ino
+++ Arduino/Lesson_05_Mood_Cue/Lesson_05_Mood_Cue.ino
@@ -7,14 +7,14 @@ int angle;
 void setup() {
   myServo.attach(9);
 
-  Serial.begin(9600);
+  Serial.begin(9700); 
 }
 
 void loop() {
   potVal = analogRead(potPin);
   Serial.print("potVal: ");
   Serial.print(potVal);
-  angle = map(potVal, 0, 1022, 0, 179);
+  angle = map(potVal, 0, 1022, 0, 180);
   Serial.print(", angle: ");
   Serial.println(angle);
 
