c8068c8507bfb7e8c25849f267c157485104ec76
Nancy Kama <70664950+nancykama@users.noreply.github.com>
2022-06-17 18:17:50 -0700

    Update Practicing_Millis___Function.ino

--- Arduino/Practicing_Millis___Function/Practicing_Millis___Function.ino
+++ Arduino/Practicing_Millis___Function/Practicing_Millis___Function.ino
@@ -7,7 +7,7 @@ int ledState = LOW;             // ledState used to set the LED
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;        // will store last time LED was updated
-
+  
 // constants won't change:
 const long interval = 2000;           // interval at which to blink (milliseconds)
 
